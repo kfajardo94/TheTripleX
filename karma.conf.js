647cf7d521fdd82f1e8946b419a9f0d6f8d7b43b
kfajardo94 <sid.kfajardo@gmail.com>
2021-06-07 11:46:48 -0600

    Renombrado de proyecto y directorio

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ecommerce'),
+      dir: require('path').join(__dirname, './coverage/the-triple-x'),
       subdir: '.',
       reporters: [
         { type: 'html' },
