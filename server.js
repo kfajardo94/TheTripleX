1babcf9725da4c22da6afa507248cb8306affd0c
kfajardo94 <sid.kfajardo@gmail.com>
2021-06-07 17:09:38 -0600

    Adecuación heroku

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/the-triple-x'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/dist/the-triple-x'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: './dist/the-triple-x/'}),
-);
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/the-triple-x/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
