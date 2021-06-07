70631e56248c449379dc90ef10b82fb677423530
kfajardo94 <sid.kfajardo@gmail.com>
2021-06-07 16:38:50 -0600

    Adecuación heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/the-triple-x'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/the-triple-x/'}),
+  res.sendFile('index.html', {root: './dist/the-triple-x/'}),
 );
 
 // Start the app by listening on the default Heroku port
