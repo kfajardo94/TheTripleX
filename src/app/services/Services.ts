31fd093c2ed9b4bc4c2ef381bbf8838b35a91246
kfajardo94 <sid.kfajardo@gmail.com>
2021-06-20 22:31:31 -0600

    Se agrega paginado a home y se intenta hacer más rápido el consumo del video

--- src/app/services/Services.ts
+++ src/app/services/Services.ts
@@ -15,7 +15,7 @@ export class Services {
   videos: Videos[] = [];
 
   // URL = 'http://localhost:8080';
-  URL = 'https://thetriplex-backend.herokuapp.com';
+  URL = '';
   filtroHeader: string;
   srcVideo: string;
 
