6903e9d0dc02290fb665e1fdd3fcd2e7e4d79d02
kfajardo94 <sid.kfajardo@gmail.com>
2021-06-23 00:22:50 -0600

    Proxy en server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const proxy = require('express-http-proxy');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/the-triple-x'));
+// app.use(express.static(__dirname + '/dist/the-triple-x'));
 
 app.use('/video', proxy('https://thetriplex-backend.herokuapp.com'));
 
